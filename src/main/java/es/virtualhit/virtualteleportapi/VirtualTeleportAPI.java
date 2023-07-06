eb513da193f8e55eac402c14e3062b1e66201730
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-06 12:45:50 +0200

    Update teleport api

--- src/main/java/es/virtualhit/virtualteleportapi/VirtualTeleportAPI.java
+++ src/main/java/es/virtualhit/virtualteleportapi/VirtualTeleportAPI.java
@@ -1,4 +1,8 @@
 package es.virtualhit.virtualteleportapi;
 
+import es.virtualhit.virtualteleportapi.teleport.TeleportService;
+
 public interface VirtualTeleportAPI {
+
+    TeleportService getTeleportService();
 }
