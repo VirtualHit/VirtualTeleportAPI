92bbf5790415ad5d1bfb4b1f7fdba520ee73a3a6
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-06 22:27:38 +0200

    Update pending teleport service

--- src/main/java/es/virtualhit/virtualteleportapi/pending/PendingTeleportService.java
+++ src/main/java/es/virtualhit/virtualteleportapi/pending/PendingTeleportService.java
@@ -7,4 +7,6 @@ public interface PendingTeleportService {
 
     Optional<PendingTeleport> getPendingTeleport(UUID uuid);
     void create(PendingTeleport pendingTeleport);
+
+    void delete(UUID uuid);
 }
