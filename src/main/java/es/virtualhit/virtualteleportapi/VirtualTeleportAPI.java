ea796688c29310ac4b038136492c0396bbd8874e
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-06 12:53:46 +0200

    Update teleport api

--- src/main/java/es/virtualhit/virtualteleportapi/VirtualTeleportAPI.java
+++ src/main/java/es/virtualhit/virtualteleportapi/VirtualTeleportAPI.java
@@ -1,8 +1,10 @@
 package es.virtualhit.virtualteleportapi;
 
+import es.virtualhit.virtualteleportapi.spawn.SpawnService;
 import es.virtualhit.virtualteleportapi.teleport.TeleportService;
 
 public interface VirtualTeleportAPI {
 
     TeleportService getTeleportService();
+    SpawnService getSpawnService();
 }
