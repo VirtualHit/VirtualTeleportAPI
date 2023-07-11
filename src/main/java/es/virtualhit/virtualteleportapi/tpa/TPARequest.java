53d17d19d7659f0fa9b20d81b644a9b3fd7fa5f0
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-11 18:16:16 +0200

    Update tpa request object

--- src/main/java/es/virtualhit/virtualteleportapi/tpa/TPARequest.java
+++ src/main/java/es/virtualhit/virtualteleportapi/tpa/TPARequest.java
@@ -1,16 +1,32 @@
 package es.virtualhit.virtualteleportapi.tpa;
 
+import es.virtualhit.virtualteleportapi.teleport.TeleportPoint;
+
 import java.util.UUID;
 
 public class TPARequest {
 
     private final UUID uuid;
+    private TeleportPoint teleportPoint;
 
     public TPARequest(UUID uuid) {
         this.uuid = uuid;
     }
 
+    public TPARequest(UUID uuid, TeleportPoint teleportPoint) {
+        this.uuid = uuid;
+        this.teleportPoint = teleportPoint;
+    }
+
     public UUID getUuid() {
         return uuid;
+    }
+
+    public TeleportPoint getTeleportPoint() {
+        return teleportPoint;
+    }
+
+    public void setTeleportPoint(TeleportPoint teleportPoint) {
+        this.teleportPoint = teleportPoint;
     }
 }
