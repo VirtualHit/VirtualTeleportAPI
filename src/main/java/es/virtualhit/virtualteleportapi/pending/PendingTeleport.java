9ef3d1fafe3f52bff52731a8148eae22c2db5ce0
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-14 19:57:23 +0200

    Update pending teleport object

--- src/main/java/es/virtualhit/virtualteleportapi/pending/PendingTeleport.java
+++ src/main/java/es/virtualhit/virtualteleportapi/pending/PendingTeleport.java
@@ -7,8 +7,9 @@ import java.util.UUID;
 public class PendingTeleport extends TeleportPoint {
 
     private final UUID uuid;
+    private final boolean silent;
 
-    public PendingTeleport(UUID uuid, TeleportPoint teleportPoint) {
+    public PendingTeleport(UUID uuid, TeleportPoint teleportPoint, boolean silent) {
         super(
                 teleportPoint.getServer(),
                 teleportPoint.getWorld(),
@@ -20,9 +21,14 @@ public class PendingTeleport extends TeleportPoint {
         );
 
         this.uuid = uuid;
+        this.silent = silent;
     }
 
     public UUID getUuid() {
         return uuid;
+    }
+
+    public boolean isSilent() {
+        return silent;
     }
 }
