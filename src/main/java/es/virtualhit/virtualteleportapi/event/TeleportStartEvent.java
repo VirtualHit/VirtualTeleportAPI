46ea3c4a3387e65eb503b6b510bff29c687819da
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-25 12:55:34 +0200

    Update teleport start event

--- src/main/java/es/virtualhit/virtualteleportapi/event/TeleportStartEvent.java
+++ src/main/java/es/virtualhit/virtualteleportapi/event/TeleportStartEvent.java
@@ -2,6 +2,7 @@ package es.virtualhit.virtualteleportapi.event;
 
 import es.virtualhit.virtualteleportapi.teleport.TeleportPoint;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
@@ -10,11 +11,12 @@ import org.jetbrains.annotations.NotNull;
  * Called when a player is going to be teleported. After the countdown and after updating the last position.
  * Before the teleport.
  */
-public class TeleportStartEvent extends PlayerEvent {
+public class TeleportStartEvent extends PlayerEvent implements Cancellable {
 
     private static final HandlerList HANDLERS = new HandlerList();
     private final TeleportPoint teleportPoint;
     private final boolean silent;
+    private boolean cancelled;
 
     public TeleportStartEvent(Player player, TeleportPoint teleportPoint, boolean silent) {
         super(player);
@@ -37,5 +39,15 @@ public class TeleportStartEvent extends PlayerEvent {
 
     public boolean isSilent() {
         return silent;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
     }
 }
